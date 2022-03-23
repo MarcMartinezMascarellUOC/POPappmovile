74bbe6d508f88b47ac0cc80c53a9a7b684db27ad
Manuel Yerbes <mayerga4@gmail.com>
2022-03-23 14:48:51 +0100

    Aproximación de las cajas

--- app/App.js
+++ app/App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import retosBanner from './assets/retosBanner.jpg';
+import Boxes from './Boxes/Boxes';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 24, marginBottom: 20, fontWeight: 'bold', color: 'dimgrey',}}>#EresElMejor</Text>
+      <Text style={{fontSize: 25, marginBottom: 25, fontWeight: 'bold', color: 'dimgrey',}}>#EresElMejor</Text>
       <Image source={retosBanner} style={{width: 500, height: 300}}/>
       <StatusBar style="auto" />
       <View style={styles.menuContainer}>
         <Text>Menú</Text>
+        <View style={styles.box}>
+          <View style={styles.inner}>
+            <Text>Box 1</Text>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <View style={styles.inner}>
+            <Text>Box 2</Text>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <View style={styles.inner}>
+            <Text>Box 3</Text>
+          </View>
+        </View>
+
+        <View style={styles.box}>
+          <View style={styles.inner}>
+            <Text>Box 4</Text>
+          </View>
+        </View>
+
       </View>
     </View>
   );
@@ -17,13 +42,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    paddingTop: 60,
     flex: 1,
     alignItems: 'center',
     backgroundColor: 'bisque',
   },
   menuContainer: {
+    width: '50%',
+    height: '50%',
+    padding: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  box: {
+    width: '50%',
+    height: '50%',
+    padding: 5,
+  },
 
+  inner: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 
 });
