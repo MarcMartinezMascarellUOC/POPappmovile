8a6825874a2fe467990cbc19a48dfee7296bdc93
Manuel Yerbes <mayerga4@gmail.com>
2022-03-24 11:16:18 +0100

    REALIZADA EL TAG V1.1 (Falta crear el componente e insertar en el, toda mi parte)

--- app/App.js
+++ app/App.js
@@ -1,40 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import retosBanner from './assets/retosBanner.jpg';
-import Boxes from './Boxes/Boxes';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text style={{fontSize: 25, marginBottom: 25, fontWeight: 'bold', color: 'dimgrey',}}>#EresElMejor</Text>
-      <Image source={retosBanner} style={{width: 500, height: 300}}/>
-      <StatusBar style="auto" />
+      
+        <Text style={{fontSize: 25, marginBottom: 25, fontWeight: 'bold', color: 'dimgrey'}}>#EresElMejor</Text>
+        <Image source={retosBanner} style={{width: 500, height: 300}}/>
+        <StatusBar style="auto" />
+
+        <View style={styles.center}>
+        </View>
+
+
       <View style={styles.menuContainer}>
-        <Text>Menú</Text>
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Text>Box 1</Text>
+        
+        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("EVOLUCIÓN")}}>
+          <View style={styles.bottomItemInner}>
+            <Text style={styles.textoBoxes}>EVOLUCIÓN</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("NUEVO RETO")}}>
+          <View style={styles.bottomItemInner}>
+            <Text style={styles.textoBoxes}>NUEVO RETO</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("PERFIL")}}>
+          <View style={styles.bottomItemInner}>
+            <Text style={styles.textoBoxes}>PERFIL</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("CONTACTAR")}}>
+          <View style={styles.bottomItemInner}>
+            <Text style={styles.textoBoxes}>CONTACTAR</Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* <View style={styles.bottomItem}>
+        <TouchableOpacity>
+          <View style={styles.bottomItemInner}>
+              <Text>Press here</Text>
+          </View>
+        </TouchableOpacity>
+        </View>
+       
+
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}>
+            
+                <Text>Press here</Text>
+            
           </View>
         </View>
 
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Text>Box 2</Text>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}>
+            <TouchableOpacity>
+              <Text>Press here</Text>
+            </TouchableOpacity>
           </View>
         </View>
 
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Text>Box 3</Text>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}>
+            <TouchableOpacity>
+              <Text>Press here</Text>
+            </TouchableOpacity>
           </View>
-        </View>
-
-        <View style={styles.box}>
-          <View style={styles.inner}>
-            <Text>Box 4</Text>
-          </View>
-        </View>
-
+        </View> */}
+      
       </View>
     </View>
   );
@@ -42,29 +81,41 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
     flex: 1,
+    paddingTop: 60,
     alignItems: 'center',
     backgroundColor: 'bisque',
+    height: '45%',
+  },
+  center: {
+    height: '5%',
+    backgroundColor: '#7fbcac',
+    zIndex: 1
   },
   menuContainer: {
-    width: '50%',
-    height: '50%',
-    padding: 5,
+    height: '46.5%',
+    backgroundColor: '#5C5C5C',
     flexDirection: 'row',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    padding: 5
   },
-  box: {
+  bottomItem: {
     width: '50%',
     height: '50%',
     padding: 5,
   },
-
-  inner: {
+  bottomItemInner: {
     flex: 1,
-    backgroundColor: '#eee',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: 'cadetblue',
+    borderRadius: 30
+  },
+  textoBoxes:{
+    flex: 1,
+    padding: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18
   }
+  
 
 });
