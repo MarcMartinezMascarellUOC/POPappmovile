c79b5dde950d42c13551139745f0ed5460747e4e
Manuel Yerbes <mayerga4@gmail.com>
2022-04-18 17:57:36 +0200

    toqueteando

--- app/App.js
+++ app/App.js
@@ -1,53 +1,74 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import retosBanner from './assets/retosBanner.jpg';
-// import { MenuComponente } from './Components/MenuComponente';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { render } from 'react-dom';
 
-//screens
-import PerfilScreen from './screens/PerfilScreen';
+function EvolucionScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Evolución</Text>
+      <Button title="Evolucion" onPress={() => navigation.navigate('Evolucion')}/>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
-  return (
+
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Evolucion" component={EvolucionScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+
+// const { navigation } = this.props.navigation;
+
+  return (  
     
     <View style={styles.container}>
-      
-        <Text style={{fontSize: 25, marginBottom: 25, fontWeight: 'bold', color: 'dimgrey'}}>#EresElMejor</Text>
-        <Image source={retosBanner} style={{width: 500, height: 300}}/>
-        <StatusBar style="auto" />
 
-        <View style={styles.center}>
-        </View>
+      <Text style={{ fontSize: 25, marginBottom: 25, fontWeight: 'bold', color: 'dimgrey' }}>#EresElMejor</Text>
+      <Image source={retosBanner} style={{ width: 500, height: 300 }} />
+      <StatusBar style="auto" />
 
-       <View style={styles.menuContainer}>
-        
-        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("EVOLUCIÓN")}}>
+      <View style={styles.center}>
+      </View>
+
+      <View style={styles.menuContainer}>
+
+        <TouchableOpacity style={styles.bottomItem} onPress={() => navigation.navigate("Evolucion")}>
           <View style={styles.bottomItemInner}>
             <Text style={styles.textoBoxes}>EVOLUCIÓN</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("NUEVO RETO")}}>
+        <TouchableOpacity style={styles.bottomItem} onPress={() => { alert("NUEVO RETO"); } }>
           <View style={styles.bottomItemInner}>
             <Text style={styles.textoBoxes}>NUEVO RETO</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("PERFIL")}}>
+        <TouchableOpacity style={styles.bottomItem} onPress={() => { alert("PERFIL"); } }>
           <View style={styles.bottomItemInner}>
             <Text style={styles.textoBoxes}>PERFIL</Text>
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.bottomItem} onPress={() => {alert("CONTACTAR")}}>
+        <TouchableOpacity style={styles.bottomItem} onPress={() => { alert("CONTACTAR"); } }>
           <View style={styles.bottomItemInner}>
             <Text style={styles.textoBoxes}>CONTACTAR</Text>
           </View>
         </TouchableOpacity>
-      
-       </View>
+
+      </View>
     </View>
+    
+    
   );
 }
 
@@ -90,3 +111,4 @@ const styles = StyleSheet.create({
   }
 
 });
+
